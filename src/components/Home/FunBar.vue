81679acf55eaa5c49a9a645ada77baa8c70d42bb
xya499 <302870488@qq.com>
2022-09-21 14:41:55 +0800

    完成Home页面

---
+++ src/components/Home/FunBar.vue
@@ -0,0 +1,71 @@
+<template>
+  <div class="funBar">
+    <div class="fun">
+      <svg class="icon" aria-hidden="true">
+        <use xlink:href="#icon-biaobaiqiang"></use>
+      </svg>
+      <span>表白墙</span>
+    </div>
+    <div class="fun">
+      <svg class="icon" aria-hidden="true">
+        <use xlink:href="#icon-chengjichaxun"></use>
+      </svg>
+      <span @click="goResults()">成绩查询</span>
+    </div>
+    <div class="fun">
+      <svg class="icon" aria-hidden="true">
+        <use xlink:href="#icon-richengjihua"></use>
+      </svg>
+      <span>日程</span>
+    </div>
+    <div class="fun">
+      <svg class="icon" aria-hidden="true">
+        <use xlink:href="#icon-bijiben"></use>
+      </svg>
+      <span>笔记</span>
+    </div>
+    <div class="fun">
+      <svg class="icon" aria-hidden="true">
+        <use xlink:href="#icon-gengduo"></use>
+      </svg>
+      <span>更多</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'FunBar',
+  methods:{
+    //点击去成绩页面
+    goResults(){
+      this.$router.push('/results')
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+.funBar{
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  width: 100%;
+  height: 50px;
+  background-color: #fff;
+  border-radius: 8px;
+  .fun{
+    display: flex;
+    flex-flow: column;
+    justify-content: center;
+    align-items: center;
+    .icon{
+      width: 30px;
+      height: 30px;
+    }
+    span{
+      font-size: 12px;
+    }
+  }
+}
+</style>
---
+++ src/components/Home/TieBa.vue
@@ -0,0 +1,101 @@
+<template>
+  <div class="tieBa">
+    <div class="tieBaTop">
+      <p class="title">表白墙</p>
+      <div class="more">
+        <span>查看更多</span>
+        <svg class="icon" aria-hidden="true">
+          <use xlink:href="#icon-jiantou"></use>
+        </svg>
+      </div>
+    </div>
+    <div class="TiebaBottom">
+      <p class="content">
+        新生9月开学，如何选择社团？首选学生会？附面试攻略！
+      </p>
+      <div class="other">
+        <span class="time">08/22 18:00</span>
+        <span>阅读数:666万+</span>
+      </div>
+    </div>
+    <div class="TiebaBottom">
+      <p class="content">
+        新生9月开学，如何选择社团？首选学生会？附面试攻略！
+      </p>
+      <div class="other">
+        <span class="time">08/22 18:00</span>
+        <span>阅读数:666万+</span>
+      </div>
+    </div>
+    <div class="TiebaBottom">
+      <p class="content">
+        新生9月开学，如何选择社团？首选学生会？附面试攻略！
+      </p>
+      <div class="other">
+        <span class="time">08/22 18:00</span>
+        <span>阅读数:666万+</span>
+      </div>
+    </div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TieBa',
+}
+</script>
+
+<style lang="less" scoped>
+.tieBa {
+  margin-top: 10px;
+  background-color: #fff;
+
+  .tieBaTop {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 5px 10px;
+    border-bottom: 1px solid rgb(240, 236, 236);
+
+    .title {
+      margin: 0;
+      font-weight: 800;
+    }
+
+    .more {
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+
+      span {
+        font-size: 12px;
+      }
+
+      .icon {
+        width: 12px;
+        height: 12px;
+      }
+    }
+  }
+
+  .TiebaBottom {
+    padding: 5px 10px;
+    border-bottom: 1px solid rgb(240, 236, 236);
+
+    .content {
+      text-align: left;
+      font-size: 14px;
+      font-weight: 700;
+    }
+
+    .other {
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      font-size: 12px;
+    }
+  }
+
+}
+</style>
