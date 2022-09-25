90dc119612a37aa3bb43e414a4d0f39156dece8d
xya499 <302870488@qq.com>
2022-09-25 19:12:51 +0800

    完成登录页面

---
+++ src/components/Login/Login.vue
@@ -0,0 +1,71 @@
+<template>
+  <div class="login">
+    <div class="logo">
+      <img src="@/assets/User/头像-男三.png" alt="">
+    </div>
+    <van-form @submit="onSubmit" class="form">
+      <van-field v-model="username" name="username" placeholder="用户名" :rules="[{ required: true }]" />
+      <van-field v-model="password" type="password" name="password" placeholder="请输入密码" :rules="[{ required: true }]" />
+      <div class="button" style="margin: 10px;">
+        <van-button round block type="info" native-type="submit">登录</van-button>
+      </div>
+    </van-form>
+  </div>
+</template>
+
+<script>
+import { mapMutations } from 'vuex';
+export default {
+  name: 'Login',
+  data() {
+    return {
+      username: '',
+      password: '',
+    };
+  },
+  methods: {
+    ...mapMutations('Login', ['getUser']),
+    onSubmit(values) {
+      this.getUser(values)
+      // console.log('submit', values);
+      this.$router.go(-1)
+      let token = 'alskhdjf1234alskdhf'
+      sessionStorage.setItem('token', token)
+    },
+  },
+}
+</script>
+
+<style lang="less" scoped>
+.login {
+  display: flex;
+  flex-flow: column;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 617px;
+
+  .logo {
+    img {
+      margin-bottom: 20px;
+      width: 100px;
+      height: 100px;
+      border-radius: 50%;
+    }
+  }
+
+  .form::v-deep {
+    width: 300px;
+
+    .van-cell {
+      margin: 10px;
+      background-color: #f5f6f8;
+      border-radius: 20px;
+    }
+  }
+
+  .button {
+    width: 100%;
+  }
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -30,6 +30,9 @@ const routes = [
         path: 'details',
         name: 'ResultDetails',
         component: () => import(/* webpackChunkName: "ResultDetails" */ '../views/ResultDetailsView.vue'),
+        meta: {
+          requireAuth: true //该路由需要登录
+        }
       }
     ]
   },
@@ -39,8 +42,16 @@ const routes = [
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "User */ '../views/UserView.vue')
-  }
+    component: () => import(/* webpackChunkName: "User */ '../views/UserView.vue'),
+    meta: {
+      requireAuth: true //该路由需要登录
+    }
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    component: () => import(/* webpackChunkName: "Login */ '../views/LoginView.vue'),
+  },
 ]
 
 const router = new VueRouter({
@@ -54,5 +65,23 @@ const originalPush = VueRouter.prototype.push
 VueRouter.prototype.push = function push(location) {
   return originalPush.call(this, location).catch(err => err)
 }
+
+//登录拦截
+router.beforeEach((to, from, next) => {
+  //判断是否需要拦截
+  if (to.meta.requireAuth) {
+    if (sessionStorage.token) {
+      //如果有token
+      next()
+    } else {
+      //如果没有token
+      next({
+        path: '/login', //返回登录页面
+      })
+    }
+  } else {
+    next()
+  }
+})
 
 export default router
